e3218eac71a754d6649c6398ee83cccfba3ef614
Pawel Bajorek <50079096+PavBajor@users.noreply.github.com>
2019-05-20 20:24:44 +0100

    Add files via upload

---
+++ WindowsFormsApp18/WindowsFormsApp18/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp18
+{
+    partial class CardsGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.randomCards = new System.Windows.Forms.Button();
+            this.sortCards = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // randomCards
+            // 
+            this.randomCards.Location = new System.Drawing.Point(48, 48);
+            this.randomCards.Name = "randomCards";
+            this.randomCards.Size = new System.Drawing.Size(154, 23);
+            this.randomCards.TabIndex = 0;
+            this.randomCards.Text = "Choose Random Cards";
+            this.randomCards.UseVisualStyleBackColor = true;
+            this.randomCards.Click += new System.EventHandler(this.randomCards_Click);
+            // 
+            // sortCards
+            // 
+            this.sortCards.Location = new System.Drawing.Point(48, 78);
+            this.sortCards.Name = "sortCards";
+            this.sortCards.Size = new System.Drawing.Size(154, 23);
+            this.sortCards.TabIndex = 1;
+            this.sortCards.Text = "Sort Cards";
+            this.sortCards.UseVisualStyleBackColor = true;
+            this.sortCards.Click += new System.EventHandler(this.sortCards_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 116);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 193);
+            this.textBox1.TabIndex = 2;
+            // 
+            // CardsGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 321);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sortCards);
+            this.Controls.Add(this.randomCards);
+            this.Name = "CardsGame";
+            this.Text = "CardsGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button randomCards;
+        private System.Windows.Forms.Button sortCards;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp18/WindowsFormsApp18/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp18
+{
+    public partial class CardsGame : Form
+    {
+        List<Card> cards = new List<Card>();
+        Random random = new Random();
+        CardsComparer cc;
+        string DisplayCards()
+        {
+            string info = "5 Random cards";
+            for (int i = 0; i < cards.Count; i++)
+            {
+                info += "\r\n" + cards[i].value + " of " + cards[i].suit;
+            }
+            return info;
+        }
+        void chooseRandomCards()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Card card = new Card((Card.Value)random.Next(13), (Card.Suit)random.Next(4));
+                if (!cards.Contains(card))
+                {
+                    cards.Add(card);
+                    card = null;
+                }
+                else
+                {
+                    chooseRandomCards();
+                }
+            }
+        }
+        public CardsGame()
+        {
+            InitializeComponent();
+
+        }
+
+        private void randomCards_Click(object sender, EventArgs e)
+        {
+            chooseRandomCards();
+            textBox1.Text = DisplayCards();
+        }
+
+        private void sortCards_Click(object sender, EventArgs e)
+        {
+            cc = new CardsComparer();
+            cards.Sort(cc);
+            textBox1.Text = DisplayCards();
+        }
+    }
+}
